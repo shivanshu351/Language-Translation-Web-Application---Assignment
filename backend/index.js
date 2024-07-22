d451e2e27aaaa532f10a2c6c7253e3a30f00f834
Shivanshu Singh <shivanshusingh71@gmail.com>
2024-07-22 23:31:45 +0530

    Configure project for deployment on Vercel

--- backend/index.js
+++ backend/index.js
@@ -9,13 +9,13 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
-app.use("/", translationRouter);
+app.use("/api", translationRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
   });
 }
 
